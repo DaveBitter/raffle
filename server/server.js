aa88e8b5ec9807d27139dc79046a0f254bce2395
Steve Jonk <stevejonk1993@gmail.com>
2024-01-05 13:11:11 +0100

    feat: add extra layer for participants in raffle object

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const { Server } = require("socket.io");
+const {Server} = require("socket.io");
 const cors = require("cors");
 
 const httpServer = http.createServer();
@@ -7,62 +7,64 @@ const httpServer = http.createServer();
 const raffles = {};
 
 const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:3000", // Replace with your frontend URL
-    methods: ["GET", "POST"],
-  },
+    cors: {
+        origin: "http://localhost:3000", // Replace with your frontend URL
+        methods: ["GET", "POST"],
+    },
 });
 
 io.on("connection", (socket) => {
-  socket.on("emit_raffle_participants", ({ raffleId }) => {
-    io.emit(`updated-participants-raffle-${raffleId}`, raffles[raffleId] || []);
-  });
+    socket.on("emit_raffle_participants", ({raffleId}) => {
+        io.emit(`updated-participants-raffle-${raffleId}`, raffles[raffleId]?.participants || []);
+    });
 
-  socket.on("join_raffle", ({ raffleId, name, userId }) => {
-    if (!raffles[raffleId]) {
-      raffles[raffleId] = [];
-    }
-
-    if (
-      raffles[raffleId].find((participant) => participant.userId === userId)
-    ) {
-      raffles[raffleId] = raffles[raffleId].map((participant) => {
-        if (participant.userId === userId) {
-          return { ...participant, socketId: socket.id };
+    socket.on("join_raffle", ({raffleId, name, userId}) => {
+        console.log(raffleId)
+        if (!raffles[raffleId]) {
+            raffles[raffleId] = {
+                participants: []
+            };
         }
 
-        return participant;
-      });
-    } else {
-      raffles[raffleId].push({ name, userId, socketId: socket.id });
-    }
+        if (
+            raffles[raffleId]?.participants?.find((participant) => participant.userId === userId)
+        ) {
+            raffles[raffleId] = raffles[raffleId].participants.map((participant) => {
+                if (participant.userId === userId) {
+                    return {...participant, socketId: socket.id};
+                }
 
-    socket.join(raffleId);
-    console.log(
-      `user with [socket-id]-${socket.id}, [user-id]-${userId} and [name]-${name} joined [raffle]-${raffleId} with ${raffles[raffleId].length} participants`
-    );
+                return participant;
+            });
+        } else {
+            raffles[raffleId].participants.push({name, userId, socketId: socket.id});
+        }
 
-    io.emit(`updated-participants-raffle-${raffleId}`, raffles[raffleId]);
-  });
+        socket.join(raffleId);
+        console.log(
+            `user with [socket-id]-${socket.id}, [user-id]-${userId} and [name]-${name} joined [raffle]-${raffleId} with ${raffles[raffleId].length} participants`
+        );
 
-  socket.on("pick_raffle_winner", ({ raffleId }) => {
-    console.log("pick_raffle_winner", raffleId);
-    const raffleParticipants = raffles[raffleId];
+        io.emit(`updated-participants-raffle-${raffleId}`, raffles[raffleId].participants);
+    });
 
-    if (raffleParticipants && !!raffleParticipants.length) {
-      const winnerIndex = Math.floor(Math.random() * raffleParticipants.length);
-      const winner = raffleParticipants[winnerIndex];
+    socket.on("pick_raffle_winner", ({raffleId}) => {
+        const raffleParticipants = raffles[raffleId]?.participants;
 
-      io.emit(`raffle_winner_${raffleId}`, winner);
-    }
-  });
+        if (raffleParticipants && !!raffleParticipants.length) {
+            const winnerIndex = Math.floor(Math.random() * raffleParticipants.length);
+            const winner = raffleParticipants[winnerIndex];
+            console.log("pick_raffle_winner", raffleId, winner);
+            io.emit(`raffle_winner_${raffleId}`, winner);
+        }
+    });
 
-  socket.on("disconnect", () => {
-    console.log("A user disconnected:", socket.id);
-  });
+    socket.on("disconnect", () => {
+        console.log("A user disconnected:", socket.id);
+    });
 });
 
 const PORT = 3001;
 httpServer.listen(PORT, () => {
-  console.log(`Socket.io server is running on port ${PORT}`);
+    console.log(`Socket.io server is running on port ${PORT}`);
 });
