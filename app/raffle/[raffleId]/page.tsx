01698105775447421d28f50ee5d4a40224a035e7
Steve Jonk <stevejonk1993@gmail.com>
2024-01-05 14:37:06 +0100

    refactor: move grid to separate comp

--- app/raffle/[raffleId]/page.tsx
+++ app/raffle/[raffleId]/page.tsx
@@ -8,6 +8,7 @@ import {getSocket} from "@/src/utils/socket";
 import {MagicWandIcon} from "@radix-ui/react-icons";
 import {Button} from "@radix-ui/themes";
 import {Participant} from "@/src/types/raffle";
+import {ParticipantsGrid} from "@/src/components/ParticipantsGrid";
 
 export default function RaffleOverview({params}: {
     children: ReactNode;
@@ -66,38 +67,3 @@ export default function RaffleOverview({params}: {
     );
 }
 
-const GRID_ROWS = 10;
-const GRID_COLS = 5;
-
-const rowAreas = Array.from({length: GRID_ROWS}, (_, i) => i + 1)
-const columnAreas = Array.from({length: GRID_COLS}, (_, i) => i + 1)
-
-const ParticipantsGrid = ({participants}: { participants: Participant[] }) => {
-    const participantsMap = useMemo(() => {
-        return participants.map((participant) => ({
-            ...participant,
-            gridArea: getRandomGridArea()
-        }))
-    }, [participants.length]);
-
-    return (
-        <div
-            className={`border-2 rounded-2xl p-4 grid grid-cols-${GRID_COLS} grid-rows-${GRID_ROWS} gap-2 w-full h-[400px]`}>
-            {participantsMap.map(({socketId, name, gridArea}) => (
-                <div key={socketId} style={{gridArea}}>{name}</div>
-            ))}
-        </div>
-    );
-}
-
-const getRandomGridArea = () => {
-    const randomRowPicker = Math.floor(Math.random() * rowAreas.length)
-    const randomColumnPicker = Math.floor(Math.random() * columnAreas.length)
-
-    const row = rowAreas[randomRowPicker]
-    rowAreas.splice(randomRowPicker, 1)
-    const col = columnAreas[randomColumnPicker]
-    columnAreas.splice(randomColumnPicker, 1)
-
-    return `${row}/${col}`;
-}
---
+++ src/components/ParticipantsGrid.tsx
@@ -0,0 +1,38 @@
+import {Participant} from "@/src/types/raffle";
+import {useMemo} from "react";
+
+const GRID_ROWS = 10;
+const GRID_COLS = 5;
+
+const rowAreas = Array.from({length: GRID_ROWS}, (_, i) => i + 1)
+const columnAreas = Array.from({length: GRID_COLS}, (_, i) => i + 1)
+
+export const ParticipantsGrid = ({participants}: { participants: Participant[] }) => {
+    const participantsMap = useMemo(() => {
+        return participants.map((participant) => ({
+            ...participant,
+            gridArea: getRandomGridArea()
+        }))
+    }, [participants.length]);
+
+    return (
+        <div
+            className={`border-2 rounded-2xl p-4 grid grid-cols-${GRID_COLS} grid-rows-${GRID_ROWS} gap-2 w-full h-[400px]`}>
+            {participantsMap.map(({socketId, name, gridArea}) => (
+                <div key={socketId} style={{gridArea}}>{name}</div>
+            ))}
+        </div>
+    );
+}
+
+const getRandomGridArea = () => {
+    const randomRowPicker = Math.floor(Math.random() * rowAreas.length)
+    const randomColumnPicker = Math.floor(Math.random() * columnAreas.length)
+
+    const row = rowAreas[randomRowPicker]
+    rowAreas.splice(randomRowPicker, 1)
+    const col = columnAreas[randomColumnPicker]
+    columnAreas.splice(randomColumnPicker, 1)
+
+    return `${row}/${col}`;
+}
